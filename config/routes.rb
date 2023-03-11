4f899516702250d2d8f50f95cf30ded0be4346c2
Shota <s.minachan0119@gmail.com>
2023-03-11 16:35:07 +0900

    member作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'static_pages/home'
+  root 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
