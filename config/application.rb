0b5d8bd9c59ce375406803ff5fe3793d9763ce48
Shota <s.minachan0119@gmail.com>
2023-03-12 11:24:45 +0900

    application.rb and locales

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,13 @@ module Wrecord
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+
+    #デフォルト日本語化
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    #ロケールパス
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
